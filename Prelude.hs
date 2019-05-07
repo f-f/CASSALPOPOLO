869d3756787e4f09a1a3ec029d5e62589c01f204
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2019-05-07 08:40:00 +0300

    Integrate Volca Bass in the setup, add some more combinators

--- Prelude.hs
+++ Prelude.hs
@@ -9,21 +9,14 @@ setbpm bpm = setcps (bpm/60/4)
 -- | Defining controls from pots on the MIDI board.
 --   Assign them like `gain q1`, etc
 maxgain = 1.3
-q1 = range 0 maxgain $ cF 0 "cc1"
-q2 = range 0 maxgain $ cF 0 "cc2"
-q3 = range 0 maxgain $ cF 0 "cc3"
-q4 = range 0 maxgain $ cF 0 "cc4"
-q5 = range 0 maxgain $ cF 0 "cc5"
-q6 = range 0 maxgain $ cF 0 "cc6"
-q7 = range 0 maxgain $ cF 0 "cc7"
-q8 = range 0 maxgain $ cF 0 "cc8"
+q idx = range 0 maxgain $ cF 0 ("cc" ++ show idx)
 
 -- | TODO: PR the change to SuperDirt
 let reverbPan :: Pattern Double -> ControlPattern
     reverbPan = pF "reverbPan"
 
 -- | Make a track mutable by the assigned pad button
-mutable padButton = selectF (cF 0 padButton) [id, const silence]
+mutable padButton = selectF (cF 0 ("t" ++ show padButton)) [id, const silence]
 
 -- | If a sample has been assigned to a cut group, then we can
 --   hack a way to stop it
@@ -43,6 +36,24 @@ let killSample sampleChannel cutGroup = do
 -- p "test" $ s "trump:4/4" # cut 10 # speed 0.5
 -- killSample "test" 10
 
+-- | Sync the tempo on the Volca with Tidal
+let syncvolca = do
+      p "midiclock" $ midicmd "midiClock*96" # s "volca"
+      threadDelay 100000
+      once $ midicmd "stop" # s "volca"
+      p "midictl" $ midicmd "start/4" # s "volca"
+      threadDelay 1000000
+      p "midictl" $ silence
+
+let sendvolca pat = p "volcanotes" $ n pat # s "volca" # midichan 0
+
+let stopvolca = p "volcanotes" $ silence
+
+-- | Fix the `hush` function so we resync the midi
+let hush_ = hush >> syncvolca
+
+-- | A better `p` that we can map to MIDI controls for volume and muting
+p_ name idx pat = p name $ mutable idx (pat # gain (q idx) # orbit (fromInteger idx))
 
 -- | Get a Markov-generated harmony of length len
 harm len = fmap Markov.render (Markov.getHarmony len)
